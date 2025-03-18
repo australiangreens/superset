cf0ead1093a59adaac217d56eb917ee1c2d9b696
Patrick Figel <pfigel@greenpeace.org>
2025-03-18 12:34:14 +0100

    GP-43741 Add afform for Superset Dashboard

---
+++ afformEntities/SupersetDashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+use CRM_Superset_ExtensionUtil as E;
+
+return [
+  'type' => 'primary',
+  'defaults' => "{}",
+  'boilerplate' => [
+    ['#tag' => 'af-field', 'name' => 'title'],
+    ['#tag' => 'af-field', 'name' => 'embed_id'],
+  ],
+  'icon' => 'fa-bar-chart'
+];
